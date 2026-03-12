16378d7a78933d209a5ea68ba0b47f51c507e9a6
NickHerzb <150080184+NickHerzb@users.noreply.github.com>
2026-03-12 17:57:36 -0500

    Adding two comments.
    Added a start-of-file comment for author, purpose, expected input, expected outputs, and error handling.
    Added a comment to the fizzBuzz function explaining the expected input and output.

--- FizzBuzzYield.py
+++ FizzBuzzYield.py
@@ -1,8 +1,31 @@
+"""
+Author: NickHerzb
+Purpose: Implementing FizzBuzz using yield and yield from in Python.
+Input: A number from 1 to int() limit.
+Output: Starting from 1:
+            if # mod 15 == 0:
+                Prints "FizzBuzz"
+            elif # mod 5 == 0:
+                Prints "Buzz"
+            elif # mod 3 == 0:
+                Prints "Fizz"
+            else:
+                Prints #
+Error Handling: If the user inputs a non-int or too large of an int then an error is printed and the program ends.
+                If the user inputs a non-positive (# <= 0) int then an error is printed and the program ends.
+"""
+
 
 class NonPositive(Exception):
     pass
 
 
+# Input: None
+# Output: This is a generator that endlessly loops from 1 to 15.
+#         If the index is % 15 then "FizzBuzz" is yielded.
+#         Else if the index is % 5 then "Buzz" is yielded.
+#         Else if the index is % 3 then "Fizz" is yielded.
+#         Otherwise, the empty string is yielded.
 def fizzBuzz():
     while True:
         for idx in range(1, 16):
